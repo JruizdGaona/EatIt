07feec314aab34861d502813a8e723bec9f75aa7
Javier <javierruizdegaona@gmail.com>
2023-02-24 20:49:33 +0100

    Cambio en la imagen del Header (menú) y funcionalidad inicial del menú

--- app/src/main/java/com/example/eatit/activities/PanelControlActivity.java
+++ app/src/main/java/com/example/eatit/activities/PanelControlActivity.java
@@ -2,9 +2,11 @@ package com.example.eatit.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,15 +16,16 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.eatit.R;
 import com.example.eatit.utils.FragmentPrincipal;
-
+import com.google.android.material.navigation.NavigationView;
 import java.util.Objects;
 
 /**
  * @author Javier Ruiz de Gaona Tre.
  */
-public class PanelControlActivity extends AppCompatActivity {
+public class PanelControlActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // Declaración de variables.
+    NavigationView navigationView;
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     ImageView logo;
@@ -36,6 +39,10 @@ public class PanelControlActivity extends AppCompatActivity {
         inicializarVariables();
         cargarRecetas();
         clickMenuLateral();
+
+        MenuItem menuItem = navigationView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+        menuItem.setChecked(true);
     }
 
     /**
@@ -46,6 +53,8 @@ public class PanelControlActivity extends AppCompatActivity {
         recetas = findViewById(R.id.recetas);
         drawerLayout = findViewById(R.id.drawer_layout);
         toolbar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.menu_lateral);
+        navigationView.setNavigationItemSelectedListener(this);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
     }
@@ -76,5 +85,38 @@ public class PanelControlActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mostrarOpcion(item);
+        return true;
+    }
+
+    private int mostrarOpcion(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.inicio:
+                return 0;
+            case R.id.nav_ingredientes2:
+                Toast.makeText(this, "Añadir Ingredientes", Toast.LENGTH_SHORT).show();
+                return 0;
+            case R.id.nav_ingredientes3:
+                Toast.makeText(this, "Ver Ingredientes", Toast.LENGTH_SHORT).show();
+                return 0;
+            case R.id.nav_recetas2:
+                Toast.makeText(this, "Recomendador", Toast.LENGTH_SHORT).show();
+                return 0;
+            case R.id.nav_recetas3:
+                Toast.makeText(this, "Ver Recetas", Toast.LENGTH_SHORT).show();
+                return 0;
+            case R.id.nav_ajustes:
+                startActivity(new Intent(PanelControlActivity.this, AjustesActivity.class));
+                return 0;
+            case R.id.nav_sesion:
+                return 0;
+            default:
+                Toast.makeText(this, "Opción no Implementada aún", Toast.LENGTH_SHORT).show();
+        }
+        return 0;
     }
 }
