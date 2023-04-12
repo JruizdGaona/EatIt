43f1a43b4a30fc65562c53ab7d05a374c8ba9083
Javier <javierruizdegaona@gmail.com>
2023-04-12 21:54:23 +0200

    MinorFix SplashScreen

--- app/src/main/java/com/example/eatit/activities/SplashScreen.java
+++ app/src/main/java/com/example/eatit/activities/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.eatit.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -13,29 +14,49 @@ import com.example.eatit.R;
 
 public class SplashScreen extends Activity {
 
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String KEY_FIRST_RUN = "firstRun";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
 
-        TextView txt = findViewById(R.id.text_app);
-        TextView txtCharge = findViewById(R.id.text_cargando);
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isFirstRun = prefs.getBoolean(KEY_FIRST_RUN, true);
 
-        TranslateAnimation an = new TranslateAnimation(00.0f, 0.0f, 1600.0f, 0.0f);
-        TranslateAnimation an2 = new TranslateAnimation(00.0f, 0.0f, 0f, 50.0f);
+        if (isFirstRun) {
+            setContentView(R.layout.splashscreen);
+            TextView txt = findViewById(R.id.text_app);
+            TextView txtCharge = findViewById(R.id.text_cargando);
 
-        an.setDuration(2000);
-        txt.startAnimation(an);
+            TranslateAnimation an = new TranslateAnimation(00.0f, 0.0f, 1600.0f, 0.0f);
+            TranslateAnimation an2 = new TranslateAnimation(00.0f, 0.0f, 0f, 50.0f);
 
-        an2.setDuration(1000);
-        an2.setRepeatCount(Animation.INFINITE);
-        an2.setRepeatMode(Animation.REVERSE);
-        txtCharge.startAnimation(an2);
+            an.setDuration(2000);
+            txt.startAnimation(an);
 
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
+            an2.setDuration(1000);
+            an2.setRepeatCount(Animation.INFINITE);
+            an2.setRepeatMode(Animation.REVERSE);
+            txtCharge.startAnimation(an2);
+
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }, 2000);
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(KEY_FIRST_RUN, true);
+            editor.apply();
+
+        } else {
             startActivity(new Intent(SplashScreen.this, LoginActivity.class));
             finish();
-        }, 2000);
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(KEY_FIRST_RUN, false);
+            editor.apply();
+        }
     }
 }
