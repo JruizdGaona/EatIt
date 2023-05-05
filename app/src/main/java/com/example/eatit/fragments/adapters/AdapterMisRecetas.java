ceaba52df817fc77cf6240c1f7c57e3b57b4128c
Javier <javierruizdegaona@gmail.com>
2023-05-06 00:31:30 +0200

    Eliminar Recetas

--- app/src/main/java/com/example/eatit/fragments/adapters/AdapterMisRecetas.java
+++ app/src/main/java/com/example/eatit/fragments/adapters/AdapterMisRecetas.java
@@ -1,22 +1,35 @@
 package com.example.eatit.fragments.adapters;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.eatit.R;
+import com.example.eatit.entities.Ingrediente;
 import com.example.eatit.entities.Receta;
 import com.example.eatit.activities.ActivityRecetas;
+import com.example.eatit.entities.Usuario;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
@@ -31,6 +44,8 @@ public class AdapterMisRecetas extends RecyclerView.Adapter<AdapterMisRecetas.My
     TextView nombre;
     private String email;
     private ShapeableImageView imagen;
+    FirebaseFirestore database;
+    CollectionReference coleccion;
 
     /**
      * Constructor del Adapter de Recetas.
@@ -41,6 +56,8 @@ public class AdapterMisRecetas extends RecyclerView.Adapter<AdapterMisRecetas.My
         this.recetas = re;
         this.context = context;
         this.email = correo;
+        database = FirebaseFirestore.getInstance();
+        coleccion = database.collection("recetas");
     }
 
     /**
@@ -73,6 +90,70 @@ public class AdapterMisRecetas extends RecyclerView.Adapter<AdapterMisRecetas.My
             intent.putExtra("email", email);
             context.startActivity(intent);
         });
+
+        holder.cv.setOnLongClickListener(view -> {
+            Dialog dialog = new Dialog(context);
+            dialog.setContentView(R.layout.card_op_recetas);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            eliminarReceta(dialog, recetas.get(position));
+            cerrarCardView(dialog);
+            dialog.show();
+            Toast.makeText(context, "Pulsación Laraga", Toast.LENGTH_SHORT).show();
+            return true;
+        });
+    }
+
+    private void eliminarReceta(Dialog dialog, Receta receta) {
+        AppCompatButton eliminar = dialog.findViewById(R.id.btn_delete);
+
+        eliminar.setOnClickListener((view) -> {
+            Task<QuerySnapshot> consulta = coleccion.whereEqualTo("nombre", receta.getNombre()).get();
+
+            consulta.addOnSuccessListener(documentSnapshots -> {
+                if (!documentSnapshots.isEmpty()) {
+                    for (DocumentSnapshot documentSnapshot : documentSnapshots.getDocuments()) {
+                        if (documentSnapshot.getString("nombre").equalsIgnoreCase(receta.getNombre())) {
+                            coleccion.document(documentSnapshot.getId()).delete().addOnSuccessListener(aVoid -> {
+                                eliminarRecetaDelUsuario(receta);
+                                Toast.makeText(context, "Receta eliminada correctamente", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }).addOnFailureListener(e -> {
+                                Toast.makeText(context, "Error al borrar la receta", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            });
+                            return;
+                        }
+                    }
+                }
+                Toast.makeText(context, "Error al borrar la receta", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }).addOnFailureListener(e -> {
+                Toast.makeText(context, "Error al borrar la receta", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            });
+        });
+    }
+
+    private void eliminarRecetaDelUsuario(Receta receta) {
+        Task<QuerySnapshot> consultaUsuario = database.collection("usuarios").whereEqualTo("correo", email).get();
+        consultaUsuario.addOnSuccessListener(documentSnapshotsUsuario -> {
+            if (!documentSnapshotsUsuario.isEmpty()) {
+                DocumentSnapshot documentSnapshotUsuario = documentSnapshotsUsuario.getDocuments().get(0);
+                Usuario usuario = documentSnapshotUsuario.toObject(Usuario.class);
+                List<Receta> recetasUsuario = usuario.getRecetasCreadas();
+
+                recetasUsuario.removeIf(i -> i.getNombre().equalsIgnoreCase(receta.getNombre()));
+                String usuarioId = documentSnapshotUsuario.getId();
+
+                database.collection("usuarios").document(usuarioId).update("recetasCreadas", recetasUsuario);
+            }
+        });
+    }
+
+    private void cerrarCardView(Dialog dialog) {
+        ImageView imageView = dialog.findViewById(R.id.cerrar_op);
+        imageView.setOnClickListener((View) -> dialog.dismiss());
     }
 
     /**
